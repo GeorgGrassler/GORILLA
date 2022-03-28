0135996c1be34a0f56159afbb1e917084bb82a49
GeorgGrassler <georg.grassler@student.tugraz.at>
2022-03-28 20:13:34 +0200

    main test file created

---
+++ EIREEN/test.f90
@@ -0,0 +1,48 @@
+program test
+    use vertex_logic_mod,  only: readin_data, check_orientation, triangles_EIRENE, shape_triangles
+    implicit none
+    integer :: i, j, k
+    integer, dimension(:,:), allocatable :: diff
+    logical, dimension(:), allocatable :: check_list, check_list2
+    logical :: check_total, test1 = .false., test2 = .true.
+    logical :: check2 = .false.
+
+    call readin_data()
+
+    VERTEX_TWIN_TEST: if (test1) then
+        if (.not. allocated(diff)) allocate(diff(shape_triangles(1), 9))
+        if (.not. allocated(check_list)) allocate(check_list(shape_triangles(1)))
+
+        do i =  1, shape_triangles(1)
+            do j = 1, shape_triangles(2)
+                do k = 1, shape_triangles(2)
+                    diff(i,(j-1)*3 + k) = abs(triangles_EIRENE(i,j) - triangles_EIRENE(i,k))
+                end do 
+            end do 
+            if (.not. any(diff(i,:) == 1)) print*, triangles_EIRENE(i,:)
+        end do
+
+        check_list = any(diff == 1,2)
+        check_total = all(check_list)
+
+        !print*, check_list
+        !print*, 'TOTAL ', check_total
+
+        if (allocated(diff)) deallocate(diff)
+        if (allocated(check_list)) deallocate(check_list)
+
+    end if VERTEX_TWIN_TEST
+
+
+    PAIR_TEST: if (test2) then
+        do i = 1, shape_triangles(1)
+            check2 = check_orientation(i)
+            if (.not. check2) then
+                print*, 'Triangle ', i, 'is oriented clockwise!'
+                stop
+            end if
+        end do
+        print*, 'All triangles are counterclockwise!'
+    end if PAIR_TEST
+
+end program test
